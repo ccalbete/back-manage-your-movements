4dcb486aafd66019638df8841e4c83995e88dbda
ccalbete <ccalbete10@gmail.com>
2021-11-06 20:04:20 -0300

    add categories data

---
+++ data/categories.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        userId: 1,
+        name: "Food",
+        isFixed: false,
+        spent: 100
+    },
+    {
+        userId: 1,
+        name: "Booking",
+        isFixed: true,
+        spent: 500,
+    },
+    {
+        userId: 2,
+        name: "Cleaning",
+        isFixed: false,
+        spent: 400
+    }
+];
