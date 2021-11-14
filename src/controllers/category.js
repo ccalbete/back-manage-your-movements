b7e30597855be4a9e1ca6c52c9b805ee376b413c
ccalbete <ccalbete10@gmail.com>
2021-11-14 18:57:26 -0300

    Remove fixedExpenses and include it in categories close #31

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -16,6 +16,21 @@ function addToSpent(userId, categoryToUpdate, amount) {
     selectedCategory.spent += amount;
 }
 
+function getFixedExpensesCategoriesByUser(userId) {
+    const userCategories = categories.filter(category => category.userId == userId);
+    const fixedExpensesCategories = userCategories.filter(category => category.isFixedExpense);
+    return fixedExpensesCategories;
+}
+
+function getNotFixedExpensesCategoriesByUser(userId) {
+    const userCategories = categories.filter(category => category.userId == userId);
+    const notFixedExpensesCategories = userCategories.filter(category => !category.isFixedExpense);
+    return notFixedExpensesCategories;
+}
+
+
 module.exports.getCategories = getCategories;
 module.exports.getCategoriesByUser = getCategoriesByUser;
 module.exports.addToSpent = addToSpent;
+module.exports.getFixedExpensesCategoriesByUser = getFixedExpensesCategoriesByUser;
+module.exports.getNotFixedExpensesCategoriesByUser = getNotFixedExpensesCategoriesByUser;
--- src/routes/categories.js
+++ src/routes/categories.js
@@ -14,5 +14,17 @@ router.get("/:userId", (req, res) => {
     res.send({ success: true, userCategories });
 });
 
+router.get("/:userId/fixedExpenses", (req, res) => {
+    const userId = req.params.userId;
+    const userFixedExpensesCategories = categoryController.getFixedExpensesCategoriesByUser(userId);
+    res.send({ success: true, userFixedExpensesCategories });
+});
+
+router.get("/:userId/notFixedExpenses", (req, res) => {
+    const userId = req.params.userId;
+    const userNotFixedExpensesCategories = categoryController.getNotFixedExpensesCategoriesByUser(userId);
+    res.send({ success: true, userNotFixedExpensesCategories });
+});
+
 
 module.exports = router;
