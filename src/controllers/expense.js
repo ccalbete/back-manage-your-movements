795ff19e6287f4fddd848404a70022db17acd89f
ccalbete <ccalbete10@gmail.com>
2022-01-13 16:08:40 -0300

    saveExpense() now receive ids by parameter from front, now isnt necessary get things id by name

--- src/controllers/expense.js
+++ src/controllers/expense.js
@@ -26,21 +26,16 @@ async function getExpensesByUser(userId) {
 async function saveExpense(userId, amount, paymentMode, place, category, date) {
     try {
 
-        const payment_mode_id = await paymentModeController.getPaymentModeId(userId, paymentMode);
-        const place_id = await placeController.getPlaceId(userId, place);
-        const category_id = await categoryController.getCategoryId(userId, category);
+        await categoryController.addToSpent(userId, category, amount);
+        await paymentModeController.addToSpent(userId, paymentMode, amount);
+
+        const is_debit = await db.query("select is_debit from payment_modes where id = $1", [paymentMode]);
+        if (is_debit.rows[0].is_debit) {
+            await paymentModeController.subtractToAvailable(userId, paymentMode, amount);
+        }
 
         await db.query("insert into expenses(user_id, amount, payment_mode_id, place_id, category_id, date) values($1, $2, $3, $4, $5, $6)",
-            [userId, amount, payment_mode_id, place_id, category_id, date]);
-
-        //if the expense was correctly saved, update available of payment mode
-        await categoryController.addToSpent(userId, category_id, amount);
-        await paymentModeController.addToSpent(userId, payment_mode_id, amount);
-
-        const is_debit = await db.query("select is_debit from payment_modes where id = $1", [payment_mode_id]);
-        if (is_debit.rows[0].is_debit) {
-            await paymentModeController.subtractToAvailable(userId, payment_mode_id, amount);
-        }
+            [userId, amount, paymentMode, place, category, date]);
         
     } catch (error) {
         throw new Error(error)
